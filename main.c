7bd22ac7a1c987ef2e59e9767c41f294ec14be3f
kadirbatmaz321-dotcom <kadirbatmaz321@gmail.com>
2026-05-09 20:55:03 +0300

    kitap ekleme yazma

--- main.c
+++ main.c
@@ -1,108 +1,106 @@
-/*******************************************************************************************
-*
-*   raylib [core] example - Basic 3d example
-*
-*   Welcome to raylib!
-*
-*   To compile example, just press F5.
-*   Note that compiled executable is placed in the same folder as .c file
-*
-*   You can find all basic examples on C:\raylib\raylib\examples folder or
-*   raylib official webpage: www.raylib.com
-*
-*   Enjoy using raylib. :)
-*
-*   This example has been created using raylib 1.0 (www.raylib.com)
-*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
-*
-*   Copyright (c) 2013-2024 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h> 
+#include <raylib.h>
 
-#include "raylib.h"
+#define Genislik_BOYUTU 1150
+#define Yükseklik_BOYUTU 700
+typedef enum { EKRAN_KULLANICI_GIRISI, EKRAN_KISI } KullaniciDurumu;
+struct kitap {
+    char isim[50];
+    int yil;
+    char yazar[50];
+} kitaplar[100];
 
-#if defined(PLATFORM_WEB)
-    #include <emscripten/emscripten.h>
-#endif
-
-//----------------------------------------------------------------------------------
-// Local Variables Definition (local to this module)
-//----------------------------------------------------------------------------------
-Camera camera = { 0 };
-Vector3 cubePosition = { 0 };
-
-//----------------------------------------------------------------------------------
-// Local Functions Declaration
-//----------------------------------------------------------------------------------
-static void UpdateDrawFrame(void);          // Update and draw one frame
-
-//----------------------------------------------------------------------------------
-// Main entry point
-//----------------------------------------------------------------------------------
-int main()
-{
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
-
-    InitWindow(screenWidth, screenHeight, "raylib");
-
-    camera.position = (Vector3){ 10.0f, 10.0f, 8.0f };
-    camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };
-    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
-    camera.fovy = 60.0f;
-    camera.projection = CAMERA_PERSPECTIVE;
-
-    //--------------------------------------------------------------------------------------
-
-#if defined(PLATFORM_WEB)
-    emscripten_set_main_loop(UpdateDrawFrame, 60, 1);
-#else
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
-
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
-    {
-        UpdateDrawFrame();
+void dosyayaYaz(struct kitap eklenecekKitap) {
+    FILE *dosya = fopen("kutuphane.txt", "a");
+    if (dosya == NULL) {
+        printf("Hata: Dosya acilamadi!\n");
+        return; 
     }
-#endif
-
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();                  // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
-
-    return 0;
+    fprintf(dosya, "%s,%d,%s\n", eklenecekKitap.isim, eklenecekKitap.yil, eklenecekKitap.yazar);
+    fclose(dosya);
 }
 
-// Update and draw game frame
-static void UpdateDrawFrame(void)
-{
-    // Update
-    //----------------------------------------------------------------------------------
-    UpdateCamera(&camera, CAMERA_ORBITAL);
-    //----------------------------------------------------------------------------------
-
-    // Draw
-    //----------------------------------------------------------------------------------
-    BeginDrawing();
-
+int main() {
+    InitWindow(Genislik_BOYUTU, Yükseklik_BOYUTU, "Kutuphane Yonetim Sistemi");
+    SetTargetFPS(60); 
+    int aktifKutu = 0; 
+    char girdiIsim[50] = "\0",girdiYazar[50] = "\0",girdiYil[10] = "\0";
+    int harfSayisiIsim = 0,harfSayisiYil = 0,harfSayisiYazar = 0;
+    while (!WindowShouldClose()) {
+        if (aktifKutu < 3) {            
+            int tus = GetCharPressed();
+            while (tus > 0) {
+                if ((tus >= 32) && (tus <= 125)) {
+                    if (aktifKutu == 0 && harfSayisiIsim < 49) {
+                        girdiIsim[harfSayisiIsim] = (char)tus;
+                        girdiIsim[harfSayisiIsim + 1] = '\0'; // Metnin sonunu kapat
+                        harfSayisiIsim++;
+                    }
+                    else if (aktifKutu == 1 && harfSayisiYil < 9) {
+                        if (tus >= '0' && tus <= '9') { // Yıl için SADECE rakamlara izin ver
+                            girdiYil[harfSayisiYil] = (char)tus;
+                            girdiYil[harfSayisiYil + 1] = '\0';
+                            harfSayisiYil++;
+                        }
+                    }
+                    else if (aktifKutu == 2 && harfSayisiYazar < 49) {
+                        girdiYazar[harfSayisiYazar] = (char)tus;
+                        girdiYazar[harfSayisiYazar + 1] = '\0';
+                        harfSayisiYazar++;
+                    }
+                }
+                tus = GetCharPressed();
+            }
+            if (IsKeyPressed(KEY_BACKSPACE)) {
+                if (aktifKutu == 0 && harfSayisiIsim > 0) {
+                    harfSayisiIsim--;
+                    girdiIsim[harfSayisiIsim] = '\0';
+                }
+                else if (aktifKutu == 1 && harfSayisiYil > 0) {
+                    harfSayisiYil--;
+                    girdiYil[harfSayisiYil] = '\0';
+                }
+                else if (aktifKutu == 2 && harfSayisiYazar > 0) {
+                    harfSayisiYazar--;
+                    girdiYazar[harfSayisiYazar] = '\0';
+                }
+            }
+            if (IsKeyPressed(KEY_ENTER)) {
+                aktifKutu++;
+                if (aktifKutu == 3) {
+                    strcpy(kitaplar[0].isim, girdiIsim);
+                    kitaplar[0].yil = atoi(girdiYil);
+                    strcpy(kitaplar[0].yazar, girdiYazar);
+                    dosyayaYaz(kitaplar[0]);
+                }
+            }
+        }
+        BeginDrawing();
         ClearBackground(RAYWHITE);
-
-        BeginMode3D(camera);
-
-            DrawCube(cubePosition, 2.0f, 2.0f, 2.0f, RED);
-            DrawCubeWires(cubePosition, 2.0f, 2.0f, 2.0f, MAROON);
-            DrawGrid(10, 1.0f);
-
-        EndMode3D();
-
-        DrawText("This is a raylib example", 10, 40, 20, DARKGRAY);
-
-        DrawFPS(10, 10);
-
-    EndDrawing();
-    //----------------------------------------------------------------------------------
+        if (aktifKutu < 3) {
+            DrawText("Yeni Kitap Ekle (Sonraki alana gecmek icin ENTER'a basin)", 50, 50, 30, DARKGRAY);
+            DrawText("Kitap Ismi:", 50, 150, 25, DARKGRAY);//kutu1
+            DrawRectangle(250, 140, 500, 45, (aktifKutu == 0) ? LIGHTGRAY : RAYWHITE);
+            DrawRectangleLines(250, 140, 500, 45, DARKGRAY);
+            DrawText(girdiIsim, 260, 150, 25, MAROON);
+            DrawText("Basim Yili:", 50, 220, 25, DARKGRAY);//kutu2
+            DrawRectangle(250, 210, 500, 45, (aktifKutu == 1) ? LIGHTGRAY : RAYWHITE);
+            DrawRectangleLines(250, 210, 500, 45, DARKGRAY);
+            DrawText(girdiYil, 260, 220, 25, MAROON);
+            DrawText("Yazar Ismi:", 50, 290, 25, DARKGRAY);//kutu3
+            DrawRectangle(250, 280, 500, 45, (aktifKutu == 2) ? LIGHTGRAY : RAYWHITE);
+            DrawRectangleLines(250, 280, 500, 45, DARKGRAY);
+            DrawText(girdiYazar, 260, 290, 25, MAROON);
+            if (aktifKutu == 0) DrawText("-> Isim yaziliyor...", 780, 150, 20, GRAY);
+            if (aktifKutu == 1) DrawText("-> Sadece sayi giriniz...", 780, 220, 20, GRAY);
+            if (aktifKutu == 2) DrawText("-> Yazar yaziliyor...", 780, 290, 20, GRAY);
+        } else {
+            DrawText("Kitap sisteme eklendi.", 50, 100, 35, DARKGREEN);
+        }
+        EndDrawing();
+    }
+    CloseWindow();
+    return 0;
 }
