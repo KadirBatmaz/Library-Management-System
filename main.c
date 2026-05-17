411f0847a7bad5bd8d78757578a29b2da36500c6
kadirbatmaz321-dotcom <kadirbatmaz321@gmail.com>
2026-05-18 02:52:38 +0300

    wUpdate
    kiralama iaide etme genel düzenlemeler hataların giderilmesi

--- main.c
+++ main.c
@@ -5,14 +5,19 @@
 
 #define Genislik_BOYUTU 1150
 #define Yükseklik_BOYUTU 700
-typedef enum { EKRAN_KULLANICI_GIRISI, EKRAN_KISI } EkranDurumu;
+typedef enum { EKRAN_KULLANICI_GIRISI,EKRAN_GIRIS,EKRAN_KISI } EkranDurumu;
 typedef enum { ADMIN,UYE } UyeDurumu;
 struct kitap {
     char isim[50];
     int yil;
     char yazar[50];
+    int musaitlik;
 } kitaplar[100];
 struct kitap aramaSonuclari[100];
+struct kullanici{
+    char isim[50];
+    int sifre;
+};
 int bulunanKitapSayisi = 0;
 
 void dosyayaYaz(struct kitap eklenecekKitap) {
@@ -21,7 +26,7 @@ void dosyayaYaz(struct kitap eklenecekKitap) {
         printf("Hata: Dosya acilamadi!\n");
         return; 
     }
-    fprintf(dosya, "%s,%d,%s\n", eklenecekKitap.isim, eklenecekKitap.yil, eklenecekKitap.yazar);
+    fprintf(dosya, "%s,%d,%s,%d\n", eklenecekKitap.isim, eklenecekKitap.yil, eklenecekKitap.yazar,eklenecekKitap.musaitlik);
     fclose(dosya);
 }
 
@@ -56,7 +61,7 @@ void kitapAra(int kriter, char *aranan) {
     char satir[150];
     struct kitap okunanKitap;
     while (fgets(satir, sizeof(satir), dosya) && bulunanKitapSayisi < 100) {
-        if (sscanf(satir, "%[^,],%d,%[^\n]", okunanKitap.isim, &okunanKitap.yil, okunanKitap.yazar) == 3) {
+        if (sscanf(satir, "%49[^,],%d,%49[^,],%d", okunanKitap.isim, &okunanKitap.yil, okunanKitap.yazar,&okunanKitap.musaitlik) == 4) {
             int eslesme = 0;
             if (kriter == 0 && strstr(okunanKitap.isim, aranan) != NULL) eslesme = 1;
             else if (kriter == 1 && okunanKitap.yil == atoi(aranan)) eslesme = 1;
@@ -69,50 +74,208 @@ void kitapAra(int kriter, char *aranan) {
     }
     fclose(dosya);
 }
+int hesapIslemi(const char *dosyaAdi, const char *girilenIsim, int girilenSifre) {
+    char satir[150];
+    struct kullanici okunan;
+    if (strcmp(dosyaAdi, "ADMIN.txt") == 0) {
+        FILE *dosya = fopen("ADMIN.txt", "r");
+        if (dosya == NULL) {
+            printf("Hata: ADMIN.txt bulunamadi!\n");
+            return 0;
+        }
+        while (fgets(satir, sizeof(satir), dosya)) {
+            if (sscanf(satir, "%49[^,],%d", okunan.isim, &okunan.sifre) == 2) {
+                if (strcmp(okunan.isim, girilenIsim) == 0) {
+                    fclose(dosya);
+                    if (okunan.sifre == girilenSifre) {
+                        return 1;
+                    } else {
+                        return 0; 
+                    }
+                }
+            }
+        }
+        fclose(dosya);
+        return 0; 
+    } 
+    else if (strcmp(dosyaAdi, "uye.txt") == 0) {
+        FILE *dosya = fopen("uye.txt", "r");
+        int isimBulundu = 0;
+        if (dosya != NULL) {
+            while (fgets(satir, sizeof(satir), dosya)) {
+                if (sscanf(satir, "%49[^,],%d", okunan.isim, &okunan.sifre) == 2) {
+                    if (strcmp(okunan.isim, girilenIsim) == 0) {
+                        isimBulundu = 1;
+                        fclose(dosya);
+                        if (okunan.sifre == girilenSifre) {
+                            return 1;
+                        } else {
+                            return 0;
+                        }
+                    }
+                }
+            }
+            fclose(dosya);
+        }
+        if (isimBulundu == 0) {
+            dosya = fopen("uye.txt", "a");
+            if (dosya != NULL) {
+                fprintf(dosya, "%s,%d\n", girilenIsim, girilenSifre);
+                fclose(dosya);
+                return 1;
+            }
+        }
+    }
+    return 0; 
+}
 
+int kirala(const char *istenenKitapAdi, const char *kullaniciAdi, int islemTuru) {
+    if (islemTuru == 1) {
+        FILE *oduncDosya = fopen("odunc.txt", "r");
+        FILE *geciciOdunc = fopen("gecici_odunc.txt", "w");
+        int yetkiliMi = 0;
+        if (oduncDosya != NULL && geciciOdunc != NULL) {
+            char oSatir[150], oKullanici[50], oKitap[50];
+            while (fgets(oSatir, sizeof(oSatir), oduncDosya)) {
+                if (sscanf(oSatir, "%49[^,],%49[^\n\r]", oKullanici, oKitap) >= 2) {
+                    if (strcmp(oKullanici, kullaniciAdi) == 0 && strcmp(oKitap, istenenKitapAdi) == 0) {
+                        yetkiliMi = 1;
+                    } else {
+                        fprintf(geciciOdunc, "%s", oSatir);
+                    }
+                } else {
+                    fprintf(geciciOdunc, "%s", oSatir);
+                }
+            }
+            fclose(oduncDosya);
+            fclose(geciciOdunc);
+            if (yetkiliMi == 1) {
+                remove("odunc.txt");
+                rename("gecici_odunc.txt", "odunc.txt");
+            } else {
+                remove("gecici_odunc.txt");
+                return -1;
+            }
+        } else {
+            if(oduncDosya) fclose(oduncDosya);
+            if(geciciOdunc) fclose(geciciOdunc);
+            return -1; 
+        }
+    }
+    FILE *dosya = fopen("kutuphane.txt", "r");
+    FILE *geciciDosya = fopen("gecici.txt", "w"); 
+    if (dosya == NULL || geciciDosya == NULL) {
+        if(dosya) fclose(dosya);
+        if(geciciDosya) fclose(geciciDosya);
+        return 0; 
+    }
+    char satir[150];
+    struct kitap okunanKitap;
+    int islemYapildi = 0; 
+    int beklenenDurum = (islemTuru == 0) ? 1 : 0;
+    int yeniDurum = (islemTuru == 0) ? 0 : 1;
+    while (fgets(satir, sizeof(satir), dosya)) {
+        if (sscanf(satir, "%49[^,],%d,%49[^,],%d", okunanKitap.isim, &okunanKitap.yil, okunanKitap.yazar, &okunanKitap.musaitlik) == 4) {
+            if (strcmp(okunanKitap.isim, istenenKitapAdi) == 0 && okunanKitap.musaitlik == beklenenDurum) {
+                okunanKitap.musaitlik = yeniDurum; 
+                fprintf(geciciDosya, "%s,%d,%s,%d\n", okunanKitap.isim, okunanKitap.yil, okunanKitap.yazar, okunanKitap.musaitlik);
+                islemYapildi = 1;
+            } else {
+                fprintf(geciciDosya, "%s", satir); 
+            }
+        } else {
+            fprintf(geciciDosya, "%s", satir);
+        }
+    }
+    fclose(dosya);
+    fclose(geciciDosya);
+    if (islemYapildi == 1) {
+        remove("kutuphane.txt");
+        rename("gecici.txt", "kutuphane.txt");
+        if (islemTuru == 0) {
+            FILE *oduncDosya = fopen("odunc.txt", "a");
+            if(oduncDosya != NULL) {
+                fprintf(oduncDosya, "%s,%s\n", kullaniciAdi, istenenKitapAdi);
+                fclose(oduncDosya);
+            }
+        }
+        return 1;
+    } else {
+        remove("gecici.txt"); 
+        return 0;
+    }
+}
 int main() {
     EkranDurumu mevcutDurum = EKRAN_KULLANICI_GIRISI;
     UyeDurumu girisYapan = UYE;
     InitWindow(Genislik_BOYUTU, Yükseklik_BOYUTU, "Kutuphane Yonetim Sistemi");
     SetTargetFPS(60); 
     int aktifKutu=0,adminDurumu=0,uyeDurumu=0,aramaKriteri=0,aramaHarfSayisi=0; 
-    char girdiIsim[50] = "\0",girdiYazar[50] = "\0",girdiYil[10] = "\0",aramaGirdisi[50]="\0";
+    char girdiIsim[50] = "\0",girdiYazar[50] = "\0",girdiYil[10] = "\0",aramaGirdisi[50]="\0",aktifKullanici[50] = "\0";;
     int harfSayisiIsim = 0,harfSayisiYil = 0,harfSayisiYazar = 0;
-    Rectangle adminButon = { 400, 250, 350, 60 };
-    Rectangle uyeButon = { 400, 350, 350, 60 };
-    Rectangle ekleButon = { 750, 120, 300, 60 };
-    Rectangle araButon = { 750, 50, 300, 60 };
-    Rectangle menuButon = { 50, 50, 150, 60 };
-    Rectangle geriButon = { 50, 120, 150, 60 };
-    Rectangle isimAra = { 50, 220, 150, 40 };
-    Rectangle yilAra = { 220, 220, 150, 40 };
-    Rectangle yazarAra = { 390, 220, 150, 40 };
+    char girisIsim[50] = "\0",girisSifre[20] = "\0"; 
+    int harfSayisiGirisIsim = 0,harfSayisiGirisSifre = 0,aktifGirisKutusu = 0,girisHatasi = 0,kiralamaMesajDurumu = -1;;
+    Rectangle adminButon = { 400, 250, 350, 60 };Rectangle uyeButon = { 400, 350, 350, 60 };Rectangle ekleButon = { 750, 120, 300, 60 };
+    Rectangle araButon = { 750, 50, 300, 60 };Rectangle menuButon = { 50, 50, 150, 60 };Rectangle geriButon = { 50, 120, 150, 60 };
+    Rectangle isimAra = { 50, 220, 150, 40 };Rectangle yilAra = { 220, 220, 150, 40 };Rectangle yazarAra = { 390, 220, 150, 40 };
     Rectangle aramaKutusu = { 50, 280, 490, 45 };
-    
-
     while (!WindowShouldClose()) {
         Vector2 farePozisyonu = GetMousePosition();
         if (mevcutDurum == EKRAN_KULLANICI_GIRISI) {
             if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
                 if (CheckCollisionPointRec(farePozisyonu, adminButon)) {
+                    memset(girisIsim, 0, sizeof(girisIsim)); memset(girisSifre, 0, sizeof(girisSifre));
+                    harfSayisiGirisIsim = 0; harfSayisiGirisSifre = 0; aktifGirisKutusu = 0; girisHatasi = 0;
                     girisYapan = ADMIN;
-                    mevcutDurum = EKRAN_KISI;
+                    mevcutDurum = EKRAN_GIRIS;
                     adminDurumu=0;
                 }
                 else if (CheckCollisionPointRec(farePozisyonu, uyeButon)) {
+                    memset(girisIsim, 0, sizeof(girisIsim)); memset(girisSifre, 0, sizeof(girisSifre));
+                    harfSayisiGirisIsim = 0; harfSayisiGirisSifre = 0; aktifGirisKutusu = 0; girisHatasi = 0;
                     girisYapan = UYE;
-                    mevcutDurum = EKRAN_KISI;
+                    mevcutDurum = EKRAN_GIRIS;
                     bulunanKitapSayisi = 0;
                     memset(aramaGirdisi, 0, sizeof(aramaGirdisi));
                     aramaHarfSayisi = 0;
                     uyeDurumu=0;
                 }
             }
-        } 
+        }
+        else if (mevcutDurum == EKRAN_GIRIS) {
+            if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && CheckCollisionPointRec(farePozisyonu, geriButon)){
+                mevcutDurum = EKRAN_KULLANICI_GIRISI;
+            }
+            if (aktifGirisKutusu == 0) {
+                metinGirisiAl(girisIsim, &harfSayisiGirisIsim, 49, 0);
+            } else if (aktifGirisKutusu == 1) {
+                metinGirisiAl(girisSifre, &harfSayisiGirisSifre, 19, 1);
+            }
+            if (IsKeyPressed(KEY_ENTER)) {
+                if (aktifGirisKutusu == 0) {
+                    aktifGirisKutusu = 1;
+                } else if (aktifGirisKutusu == 1) {
+                    int sifreInt = atoi(girisSifre);
+                    const char* dosyaAdi = (girisYapan == ADMIN) ? "ADMIN.txt" : "uye.txt";
+                    if (hesapIslemi(dosyaAdi, girisIsim, sifreInt) == 1) {
+                        mevcutDurum = EKRAN_KISI;
+                        strcpy(aktifKullanici, girisIsim);
+                        adminDurumu = 0; 
+                        uyeDurumu = 0;
+                    } else {
+                        girisHatasi = 1;
+                        memset(girisSifre, 0, sizeof(girisSifre));
+                        harfSayisiGirisSifre = 0;
+                        aktifGirisKutusu=0;
+                    }
+                }
+            }
+        }
         else if (mevcutDurum == EKRAN_KISI) {
             if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && CheckCollisionPointRec(farePozisyonu, menuButon)){
                 mevcutDurum = EKRAN_KULLANICI_GIRISI;}
             if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && CheckCollisionPointRec(farePozisyonu, geriButon)){
+                memset(girdiIsim, 0, sizeof(girdiIsim));memset(girdiYil, 0, sizeof(girdiYil));memset(girdiYazar, 0, sizeof(girdiYazar));
                 if((girisYapan == ADMIN && adminDurumu == 1)||(girisYapan == ADMIN && adminDurumu == 2)) {
                     adminDurumu = 0;
                 } else if((girisYapan == UYE && uyeDurumu == 1)) {
@@ -124,7 +287,6 @@ int main() {
                 if(adminDurumu==0){
                     if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && CheckCollisionPointRec(farePozisyonu, ekleButon)) {
                         adminDurumu = 1; aktifKutu = 0;
-                        memset(girdiIsim, 0, sizeof(girdiIsim));memset(girdiYil, 0, sizeof(girdiYil));memset(girdiYazar, 0, sizeof(girdiYazar));
                         harfSayisiIsim = 0; harfSayisiYil = 0; harfSayisiYazar = 0;
                     }
                     if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && CheckCollisionPointRec(farePozisyonu, araButon)) {
@@ -145,6 +307,7 @@ int main() {
                                 strcpy(kitaplar[0].isim, girdiIsim);
                                 kitaplar[0].yil = atoi(girdiYil);
                                 strcpy(kitaplar[0].yazar, girdiYazar);
+                                kitaplar[0].musaitlik = 1;
                                 dosyayaYaz(kitaplar[0]);
                             }
                         }
@@ -157,11 +320,30 @@ int main() {
                     }
                 }
             }
-            if ((girisYapan == UYE && uyeDurumu==1) || (girisYapan == ADMIN && adminDurumu == 2)) {
+            if ((girisYapan == UYE && uyeDurumu==0) || (girisYapan == ADMIN && adminDurumu == 2)) {
                 if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
                     if (CheckCollisionPointRec(farePozisyonu, isimAra)) aramaKriteri = 0;
                     if (CheckCollisionPointRec(farePozisyonu, yilAra)) aramaKriteri = 1;
                     if (CheckCollisionPointRec(farePozisyonu, yazarAra)) aramaKriteri = 2;
+                    if (girisYapan == UYE) {
+                        for (int i = 0; i < bulunanKitapSayisi; i++) {
+                            if (i > 10) break;
+                            Rectangle satirButonu = { 950, 400 + (i * 25) - 2, 85, 20 };
+                            if (CheckCollisionPointRec(farePozisyonu, satirButonu)) {
+                                if (aramaSonuclari[i].musaitlik == 1) { 
+                                    int sonuc = kirala(aramaSonuclari[i].isim, aktifKullanici, 0);
+                                    kiralamaMesajDurumu = (sonuc == 1) ? 1 : 0;
+                                } else { 
+                                    int sonuc = kirala(aramaSonuclari[i].isim, aktifKullanici, 1);
+                                    if (sonuc == 1) kiralamaMesajDurumu = 2;
+                                    else if (sonuc == -1) kiralamaMesajDurumu = -1; 
+                                    else kiralamaMesajDurumu = 0;
+                                }
+                                kitapAra(aramaKriteri, aramaGirdisi); 
+                                break; 
+                            }
+                        }
+                    }
                 }
                 int sadeceSayiMi = (aramaKriteri == 1) ? 1 : 0;
                 metinGirisiAl(aramaGirdisi, &aramaHarfSayisi, 49, sadeceSayiMi);
@@ -180,6 +362,24 @@ int main() {
             DrawRectangleRec(uyeButon, LIGHTGRAY);
             DrawRectangleLinesEx(uyeButon, 2, DARKGRAY);
             DrawText("UYE Girisi", uyeButon.x + 45, uyeButon.y + 15, 25, BLACK);
+        }else if (mevcutDurum == EKRAN_GIRIS) {
+            DrawRectangleRec(geriButon, LIGHTGRAY);
+            DrawText("GERI", geriButon.x + 35, geriButon.y + 15, 25, BLACK);
+            const char* baslik = (girisYapan == ADMIN) ? "ADMIN GIRISI" : "UYE GIRISI / KAYDI";
+            DrawText(baslik, 400, 150, 30, DARKBLUE);
+            DrawText("Kullanici Adi:", 250, 250, 25, DARKGRAY);
+            DrawRectangle(450, 240, 300, 45, (aktifGirisKutusu == 0) ? LIGHTGRAY : RAYWHITE);
+            DrawRectangleLines(450, 240, 300, 45, DARKGRAY);
+            DrawText(girisIsim, 460, 250, 25, MAROON);
+            DrawText("Sifre (Sayi):", 250, 320, 25, DARKGRAY);
+            DrawRectangle(450, 310, 300, 45, (aktifGirisKutusu == 1) ? LIGHTGRAY : RAYWHITE);
+            DrawRectangleLines(450, 310, 300, 45, DARKGRAY);
+            DrawText(girisSifre, 460, 320, 25, MAROON); 
+            DrawText("Sonraki kutu / Giris icin ENTER'a basin", 350, 400, 20, GRAY);
+            if (girisHatasi == 1&&girisYapan==ADMIN) {
+                DrawText(" Sifre yanlis veya Admin bulunamadi!", 350, 450, 25, RED);
+            }else if(girisHatasi == 1&&girisYapan==UYE)
+                DrawText(" Sifre yanlis veya UYE bulunamadi!", 350, 450, 25, RED);
         }else if(mevcutDurum == EKRAN_KISI) {
             DrawRectangleRec(menuButon, LIGHTGRAY);
             DrawText("MENU", menuButon.x + 35, menuButon.y + 15, 25, BLACK);
@@ -223,18 +423,27 @@ int main() {
                 DrawText(aramaGirdisi, aramaKutusu.x + 10, aramaKutusu.y + 10, 25, MAROON);
                 DrawText("Aramak icin metni yazip ENTER'a basin...", 560, 290, 20, GRAY);
                 DrawText("BULUNAN KITAPLAR:", 50, 350, 25, DARKGREEN);
-                DrawLine(50, 380, 1100, 360, DARKGRAY);
+                DrawLine(50, 380, 1100, 380, DARKGRAY);
                 for (int i = 0; i < bulunanKitapSayisi; i++) {
                     if (i > 10) break;
                     char sonucMetni[150];
-                    sprintf(sonucMetni, "Kitap: %-30s | Yazar: %-25s | Yil: %d", aramaSonuclari[i].isim, aramaSonuclari[i].yazar, aramaSonuclari[i].yil);
-                    DrawText(sonucMetni, 50,400 + (i * 25), 20, BLACK);
+                    const char* durumMetni = (aramaSonuclari[i].musaitlik == 1) ? "MUSAIT" : "KIRALANDI";
+                    sprintf(sonucMetni, "Kitap: %-30s | Yazar: %-20s | Yil: %-20d | DURUM: %-20s", aramaSonuclari[i].isim, aramaSonuclari[i].yazar, aramaSonuclari[i].yil,durumMetni);
+                    Color metinRengi = (aramaSonuclari[i].musaitlik == 1) ? DARKGREEN : MAROON;
+                    DrawText(sonucMetni, 50, 400 + (i * 25), 20, metinRengi);
                 }
                 if (bulunanKitapSayisi == 0 && aramaHarfSayisi > 0) {
                     DrawText("Sonuc bulunamadi veya aranmadi...", 50, 400, 20, DARKGRAY);
                 }
             }
+            if (girisYapan == UYE) {
+                if (kiralamaMesajDurumu == 1) DrawText("Kitap basariyla kiralandi!", 560, 350, 20, DARKGREEN);
+                else if (kiralamaMesajDurumu == 2) DrawText("Kitap basariyla iade edildi!", 560, 350, 20, BLUE);
+                else if (kiralamaMesajDurumu == -1) DrawText("HATA: Bu kitabi siz kiralamadiniz!", 560, 350, 20, MAROON); // YENİ
+                else if (kiralamaMesajDurumu == 0) DrawText("Islem basarisiz!", 560, 350, 20, MAROON);
+                }
         }
+
         EndDrawing();
     }
     CloseWindow();
